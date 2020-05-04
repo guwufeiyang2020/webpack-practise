cef6366e6dc32f354114836a4a80d721c6640d23
guwufeiyang2020 <444980149@qq.com>
2020-05-04 15:57:24 +0800

    feat: 文件修改

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="container">
     <h1>{{ initData }}</h1>
@@ -21,27 +20,3 @@ export default {
   display: flex;
 }
 </style>
-=======
-<template>
-    <div class="container">
-        <h1>{{initData}}</h1>
-    </div>
-</template>
-
-<script>
-export default {
-    data() {
-        return {
-            initData: 'vue开发环境运行成功'
-        }
-    }
-}
-</script>
-
-<style scoped>
-.container {
-    color: red;
-    display: flex;
-}
-</style>
->>>>>>> 8ca7a6d... feat: 初始化仓库
